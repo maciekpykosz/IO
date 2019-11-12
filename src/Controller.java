dbf19a79ed94301a3d7d8070a663897475408d25
Agata Pietrowska <48550541+agatapietri@users.noreply.github.com>
2019-11-12 16:06:06 +0100

    Java FX classes

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,107 @@
-public class Controller {
+import org.gummie_bears.io_project.model.DependencyFinder;
+import org.gummie_bears.io_project.model.DependencyObj;
+import org.gummie_bears.io_project.model.EdgeSettings;
+import org.gummie_bears.io_project.model.GraphDraw;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+
+import java.io.File;
+import java.util.List;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+public class Controller  {
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private MenuItem fileDepMenu;
+    @FXML
+    private MenuItem methodDepMenu;
+    @FXML
+    private MenuItem packageDepMenu;
+    private GraphDraw graphDraw;
+    @FXML
+    private Label viewingInfo;
+    @FXML
+    private ImageView imageLabel;
+    private Image image;
+    private Stage onCreatedStage;
+
+    private DependencyFinder dependencyFinder;
+    private DirectoryChooser directoryChooser;
+    private final static String fileNameForFirstGraph = "graf1";
+    private final static String fileNameForSecondGraph = "graf2";
+    private final static String fileNameForThirdGraph = "graf3";
+
+    public Controller ()
+    {
+        super();
+        dependencyFinder = new DependencyFinder();
+        graphDraw = new GraphDraw();
+        directoryChooser = new DirectoryChooser();
+    }
+
+    public void setStage(Stage onCreatedStage) {
+        this.onCreatedStage = onCreatedStage;
+    }
+    public void loadFileDep(ActionEvent actionEvent) {
+
+        File selectedDir = directoryChooser.showDialog(onCreatedStage);
+        if(selectedDir != null)
+        {
+            List<DependencyObj> dependencyObjs = dependencyFinder.getFilesDependencies(selectedDir.getAbsolutePath());
+            DefaultDirectedWeightedGraph<DependencyObj, EdgeSettings> g1 = graphDraw.getGraphForDependencies(dependencyObjs);
+            ControllerFunctions.saveGraphImage(fileNameForFirstGraph, g1);
+            File imageFile1 = new File(System.getProperty("user.dir").toString()+ "/src/main/resources/" + fileNameForFirstGraph + ".png");
+            viewingInfo.setText("Showing classs dependencies");
+
+            ControllerFunctions.loadingImage(imageFile1, imageLabel, image, gridPane);
+        }
+    }
+
+    public void loadMethodDep(ActionEvent actionEvent) {
+        File selectedDir = directoryChooser.showDialog(onCreatedStage);
+        if(selectedDir != null)
+        {
+            List<DependencyObj> dependencyObjs = dependencyFinder.getMethodsDependencies(selectedDir.getAbsolutePath());
+            DependencyObj.calculateWeightsForMethods(dependencyObjs);
+            DefaultDirectedWeightedGraph<DependencyObj, EdgeSettings> g2 = graphDraw.getGraphForDependencies(dependencyObjs);
+            ControllerFunctions.saveGraphImage(fileNameForSecondGraph, g2);
+            File imageFile2 = new File(System.getProperty("user.dir").toString() + "/src/main/resources/" + fileNameForSecondGraph + ".png");
+            viewingInfo.setText("Showing method dependencies");
+
+            ControllerFunctions.loadingImage(imageFile2, imageLabel, image, gridPane);
+        }
+    }
+
+    public void loadPackageDep(ActionEvent actionEvent) {
+        File selectedDir = directoryChooser.showDialog(onCreatedStage);
+        if(selectedDir != null)
+        {
+            List<DependencyObj> dependencyObjs = dependencyFinder.getModuleDependencies(selectedDir.getAbsolutePath());
+            DependencyObj.calculateWeightsForMethods(dependencyObjs);
+            DefaultDirectedWeightedGraph<DependencyObj, EdgeSettings> g3 = graphDraw.getGraphForDependencies(dependencyObjs);
+            ControllerFunctions.saveGraphImage(fileNameForThirdGraph, g3);
+            File imageFile3 = new File(System.getProperty("user.dir").toString() + "/src/main/resources/" + fileNameForThirdGraph + ".png");
+            viewingInfo.setText("Showing module dependencies");
+
+            ControllerFunctions.loadingImage(imageFile3, imageLabel, image, gridPane);
+        }
+    }
+
+    public void closeApp(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void showAppInfo(ActionEvent actionEvent) {
+        ControllerFunctions.infoAlert();
+    }
 }
---
+++ src/ControllerFunctions.java
@@ -0,0 +1,76 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ControllerFunctions {
+    private static Stage onCreatedStage;
+    public static void saveGraphImage(String imageName, DefaultDirectedWeightedGraph<DependencyObj, EdgeSettings> g){
+        File imgFile = new File(System.getProperty("user.dir").toString() + "/src/main/resources/" + imageName + ".png");
+        try {
+            imgFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JGraphXAdapter<DependencyObj, EdgeSettings> graphAdapter = new JGraphXAdapter<>(g);
+
+        mxIGraphLayout layout = new mxHierarchicalLayout(graphAdapter);
+        layout.execute(graphAdapter.getDefaultParent());
+
+        BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, java.awt.Color.WHITE , true, null);
+        try {
+            ImageIO.write(image, "PNG", imgFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //converting bufferedImage to image in javaFX in Java8
+    private static Image convertToFxImage(BufferedImage image) {
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+
+        return new ImageView(wr).getImage();
+    }
+
+    public static void loadingImage(File imageFile, ImageView imageLabel, Image image, GridPane gridPane){
+        try {
+            image = convertToFxImage(ImageIO.read(imageFile));
+
+            imageLabel.setImage(image);
+            imageLabel.setFitHeight(gridPane.getHeight());
+            imageLabel.setFitWidth(gridPane.getWidth());
+            imageLabel.fitHeightProperty().bind(gridPane.heightProperty());
+            //icon = new ImageView(icon.getImage());
+            System.out.println(" X: " + imageLabel.getScaleX() + " Y: " + imageLabel.getScaleY() + " Width: " + imageLabel.getImage().getWidth() + "Height: " + imageLabel.getImage().getHeight());
+        }catch (IOException e) {
+            System.err.println("Blad odczytu obrazka");
+            e.printStackTrace();
+        }
+    }
+    public static void infoAlert() {
+        Alert info = new Alert(Alert.AlertType.INFORMATION);
+        info.setTitle("Info");
+        info.setHeaderText("Information about this program");
+        info.setContentText("Dependency analyser made by \"Gumisie\"");
+        info.showAndWait();
+    }
+}
